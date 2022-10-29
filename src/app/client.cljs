032494be4c55d41d65cfac5f77e51689485ecd06
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2022-10-29 21:15:35 +0100

    Video 3 - add mutations

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -3,21 +3,35 @@
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom]
+    [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [com.fulcrologic.fulcro.algorithms.merge :as merge]))
 
 (defsc Car [this {:car/keys [id model]}]
-  {:query [:car/id :car/model]
-   :ident :car/id}
+  {:query         [:car/id :car/model]
+   :ident         :car/id
+   :initial-state {:car/id    :param/id
+                   :car/model :param/model}}
   (dom/div "Model: " model))
 
 (def ui-car (comp/factory Car {:keyfn :car/id}))
 
+(defmutation make-older [{:person/keys [id]}]
+  (action [{:keys [state]}]
+          (swap! state update-in [:person/id id :person/age] inc)))
+
 (defsc Person [this {:person/keys [id name age cars] :as props}]
-  {:query [:person/id :person/name :person/age {:person/cars (comp/get-query Car)}]
-   :ident :person/id}
+  {:query         [:person/id :person/name :person/age {:person/cars (comp/get-query Car)}]
+   :ident         :person/id
+   :initial-state {:person/id   :param/id
+                   :person/name :param/name
+                   :person/age  20
+                   :person/cars [{:id 40 :model "Leaf"}
+                                 {:id 41 :model "Escort"}
+                                 {:id 42 :model "Sienna"}]}}
   (dom/div
     (dom/div "Names: " name)
     (dom/div "Age: " age)
+    (dom/button {:onClick #(comp/transact! this [(make-older {:person/id id})])} "Make older")
     (dom/h3 "Cars:")
     (dom/ul
       (map ui-car cars))))
@@ -25,7 +39,8 @@
 (def ui-person (comp/factory Person {:keyfn :person/id}))
 
 (defsc Sample [this {:root/keys [person] :as props}]
-  {:query [{:root/person (comp/get-query Person)}]}
+  {:query         [{:root/person (comp/get-query Person)}]
+   :initial-state {:root/person {:id 1 :name "Bob"}}}
   (ui-person person))
 
 (defonce APP (app/fulcro-app))
@@ -33,13 +48,18 @@
 (defn ^:export init []
   (app/mount! APP Sample "app"))
 
-
 (comment
   (keys APP)
   (-> APP (::app/state-atom) deref)
 
+  (app/current-state APP)
+
+  (app/schedule-render! APP)
+
   (reset! (::app/state-atom APP) {})
-  (reset! (::app/state-atom APP) {:person/id 1 :person/name "Joe" :person/cars [{:car/id 22 :car/model "Escort"}]})
+  (do
+    (reset! (::app/state-atom APP) {:person/id 1 :person/name "Joe" :person/cars [{:car/id 22 :car/model "Escort"}]})
+    (app/schedule-render! APP))
 
   (merge/merge-component! APP Person {:person/id 1 :person/name "Joe"})
   (merge/merge-component! APP Person {:person/id 2 :person/name "Sally"})
@@ -51,9 +71,20 @@
   (merge/merge-component! APP Car {:car/id 42 :car/model "F-125"})
   (merge/merge-component! APP Person {:person/id 5 :person/cars [[:car/id 22] [:car/id 42]]})
   (merge/merge-component! APP Car {:car/id 45 :car/model "F-124"} :append [:person/id 5 :person/cars])
-  (swap! (::app/state-atom APP) assoc-in [:person/id 5 :person/age] 22)
+  (do
+    (swap! (::app/state-atom APP) assoc-in [:person/id 5 :person/age] 25)
+    (app/schedule-render! APP))
 
-  (comp/get-ident Car {:car/id 1})
+  (comp/get-ident Car {:car/id 22})
+  (comp/get-query Sample)
+  (comp/get-initial-state Person {:id 2 :name "Bob"})
+  (comp/get-initial-state Sample))
+
+(comment
+  (merge/merge-component! APP Person {:person/id 1 :person/name "Joe" :person/age 20} :replace [:root/person])
+
+  (make-older {:person/id 1})
+  `(make-older {:person/id 1})
 
   (app/current-state APP)
-  (app/schedule-render! APP))
+  (comp/transact! APP [(make-older {:person/id 1})]))
