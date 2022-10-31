46ded7ca5165c90a7a71ca3dc680ea0b489be6cd
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2022-10-31 11:14:41 +0000

    Video 4 - npm react component

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -1,10 +1,14 @@
 (ns app.client
   (:require
+    ["react-number-format" :refer (NumericFormat)]
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+    [com.fulcrologic.fulcro.algorithms.react-interop :as interop]
     [com.fulcrologic.fulcro.dom :as dom :refer [button div h3 label ul]]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [com.fulcrologic.fulcro.algorithms.merge :as merge]))
+
+(def ui-number-format (interop/react-factory NumericFormat))
 
 (defsc Car [this {:car/keys [id model]}]
   {:query         [:car/id :car/model]
@@ -42,6 +46,9 @@
              (div :.field {}
                   (label {} "Name: ")
                   name)
+             (div :.field {}
+               (label {} "Amount: ")
+               (ui-number-format {:thousandSeparator true :prefix "$"}))
              (div :.field {}
                   (label {:onClick onClick} "Age: ")
                   age)
