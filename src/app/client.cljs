72029a01ed49b9b3fc5e740038119213fb817abf
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2023-01-12 22:27:34 +0000

    part-4: Assign singleton ident (e.g. people list)

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -54,10 +54,25 @@
 
 (def ui-person (comp/factory Person {:keyfn :person/id}))
 
-(defsc Sample [this {:root/keys [person] :as props}]
-  {:query         [{:root/person (comp/get-query Person)}]
-   :initial-state {:root/person {:id 1 :name "Bob"}}}
-  (ui-person person))
+;; We have only one PersonList in the app, therefore we don't need an id for the list.
+(defsc PersonList [this {:person-list/keys [people] :as props}]
+  {:query         [{:person-list/people (comp/get-query Person)}]
+   :ident         (fn [_ _] [:component/id ::person-list])
+   :initial-state {:person-list/people [{:id 1 :name "Bob"}
+                                        {:id 2 :name "Sally"}]}}
+  (div {}
+    (h3 {} "People")
+    (map ui-person people)))
+
+
+(def ui-person-list (comp/factory PersonList))
+
+(defsc Sample [this {:root/keys [people] :as props}]
+  {:query         [{:root/people (comp/get-query PersonList)}]
+   :initial-state {:root/people {}}}
+  (div {}
+    (when people
+      (ui-person-list people))))
 
 (defonce APP (app/fulcro-app {}))
 
@@ -82,26 +97,35 @@
      (app/schedule-render! APP))
 
  ;; set state arbitrarily & request render
- (do (reset! (::app/state-atom APP) {:person/id   {1 {:person/id 1 :person/name "Joe" :person/cars [[:car/id 1]]}}
-                                     :car/id      {1 {:car/id 1 :car/model "F-124"}}
-                                     :root/person [:person/id 1]})
+ (do (reset! (::app/state-atom APP) {:root/list    [:component/id :app.client/person-list]
+                                     :component/id {::person-list {:person-list/people [[:person/id 1]]}}
+                                     :person/id    {1 {:person/id 1 :person/name "Joe" :person/cars [[:car/id 1]]}}
+                                     :car/id       {1 {:car/id 1 :car/model "F-124"}}})
      (app/schedule-render! APP))
 
  ;; usage of merge-component!
+ ;; - reset the state
  (reset! (::app/state-atom APP) {})
+ ;; - change names of default people
  (merge/merge-component! APP Person {:person/id 1 :person/name "Joe"})
- (merge/merge-component! APP Person {:person/id 2 :person/name "Sally"})
- (merge/merge-component! APP Person {:person/id 7 :person/name "Sevent"})
- (merge/merge-component! APP Person {:person/id 4 :person/name "Billy" :person/cars [{:car/id 22 :car/model "Escort"}]})
- (merge/merge-component! APP Person {:person/id 5 :person/name "Kasia"}
-                         :replace [:root/person])
- (merge/merge-component! APP Person {:person/id 5 :person/name "Kasia" :person/cars [[:car/id 22]]})
- (merge/merge-component! APP Car {:car/id 42 :car/model "F-125"})
- (merge/merge-component! APP Person {:person/id 5 :person/cars [[:car/id 22] [:car/id 42]]})
+ (merge/merge-component! APP Person {:person/id 2 :person/name "Sam"})
+ ;; - add Kasia and add her to list
+ (merge/merge-component! APP Person {:person/id 5 :person/name "Kasia" :person/cars [{:car/id 22 :car/model "Escort"}]}
+                         :prepend [:component/id ::person-list :person-list/people])
+ ;; - add to her more cars
+ (merge/merge-component! APP Person {:person/id 5 :person/cars [[:car/id 22] {:car/id 42 :car/model "F-125"}]})
  (merge/merge-component! APP Car {:car/id 45 :car/model "F-124"}
                          :append [:person/id 5 :person/cars])
- (merge/merge-component! APP Person {:person/id 5 :person/name "Kasia" :person/age 24})
- (swap! (::app/state-atom APP) update-in [:person/id 5 :person/age] inc)
+ ;; - make her the only person in the list
+ (merge/merge-component! APP PersonList {:person-list/people [[:person/id 5]]})
+ (merge/merge-component! APP PersonList {:person-list/people [{:person/id 5 :person/name "Kasia"}]}) ;; same as above
+ ;; - replace the list with Greg
+ (merge/merge-component! APP PersonList {:person-list/people [{:person/id 6 :person/name "Greg" :person/cars [{:car/id 1033 :car/model "Toyota"}]}]})
+ (merge/merge-component! APP PersonList {:person-list/people [{:person/id 6 :person/name "Greg" :person/cars [{:car/id 1033 :car/model "Toyota"}]}]}
+                         :replace [:root/list])             ;; in this particular case this :replace do nothing because PersonList is singleton
+ ;; - add one car to Greg
+ (merge/merge-component! APP Car {:car/id 45 :car/model "F-124"}
+                         :append [:person/id 6 :person/cars])
 
  ;; get ident
  (comp/get-ident Car {:car/id 1})
