9e351912140f944e4137dfadb4e74179814f4a8b
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2023-01-17 10:50:04 +0000

    part-6: Remove code redundant for this video

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -1,16 +1,10 @@
 (ns app.client
   (:require
-   ["react-number-format" :refer (NumericFormat)]
-   [com.fulcrologic.fulcro.algorithms.denormalize :as fdn]
-   [com.fulcrologic.fulcro.algorithms.merge :as merge]
    [com.fulcrologic.fulcro.algorithms.react-interop :as interop]
    [com.fulcrologic.fulcro.application :as app]
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
    [com.fulcrologic.fulcro.dom :as dom :refer [button div h3 label ul]]
-   [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
-   [com.fulcrologic.fulcro.rendering.ident-optimized-render :as ident-optimized]))
-
-(def ui-number-format (interop/react-factory NumericFormat))
+   [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]))
 
 (defsc Car [this {:car/keys [id model]}]
   {:query         [:car/id :car/model]
@@ -50,11 +44,6 @@
           (label {} "Name: ")
           name)
         (div :.field {}
-          (label {} "Amount:")
-          (ui-number-format {:value             "100000200.00" ;; values in HTML components are always strings
-                             :thousandSeparator true
-                             :prefix            "$"}))
-        (div :.field {}
           (label {:onClick onClick} "Age: ")
           age))
       (button {:onClick #(do (comp/transact! this [(make-older {:person/id id})] {:refresh [:person-list/people]})
@@ -77,8 +66,7 @@
     (div :.ui.segment {}
       (h3 :.ui.header "People")
       (div {} "Over 30: " cnt)
-      (div {}
-        (h3 {} "People")
+      (ul {}
         (map ui-person people)))))
 
 
@@ -102,83 +90,4 @@
   (app/mount! APP Root "app")
   (js/console.log "Hot reload"))
 
-
-(comment
- (keys APP)
-
- ;; check state
- (-> APP (::app/state-atom) deref)
- (app/current-state APP)
-
- ;; clear state & render
- (do (reset! (::app/state-atom APP) {})
-     (app/schedule-render! APP))
-
- ;; set state arbitrarily & request render
- (do (reset! (::app/state-atom APP) {:root/list    [:component/id :app.client/person-list]
-                                     :component/id {::person-list {:person-list/people [[:person/id 1]]}}
-                                     :person/id    {1 {:person/id 1 :person/name "Joe" :person/cars [[:car/id 1]]}}
-                                     :car/id       {1 {:car/id 1 :car/model "F-124"}}})
-     (app/schedule-render! APP))
-
- ;; usage of merge-component!
- ;; - reset the state
- (reset! (::app/state-atom APP) {})
- ;; - change names of default people
- (merge/merge-component! APP Person {:person/id 1 :person/name "Joe"})
- (merge/merge-component! APP Person {:person/id 2 :person/name "Sam"})
- ;; - add Kasia and add her to list
- (merge/merge-component! APP Person {:person/id 5 :person/name "Kasia" :person/cars [{:car/id 22 :car/model "Escort"}]}
-                         :prepend [:component/id ::person-list :person-list/people])
- ;; - add to her more cars
- (merge/merge-component! APP Person {:person/id 5 :person/cars [[:car/id 22] {:car/id 42 :car/model "F-125"}]})
- (merge/merge-component! APP Car {:car/id 45 :car/model "F-124"}
-                         :append [:person/id 5 :person/cars])
- ;; - make her the only person in the list
- (merge/merge-component! APP PersonList {:person-list/people [[:person/id 5]]})
- (merge/merge-component! APP PersonList {:person-list/people [{:person/id 5 :person/name "Kasia"}]}) ;; same as above
- ;; - replace the list with Greg
- (merge/merge-component! APP PersonList {:person-list/people [{:person/id 6 :person/name "Greg" :person/cars [{:car/id 1033 :car/model "Toyota"}]}]})
- (merge/merge-component! APP PersonList {:person-list/people [{:person/id 6 :person/name "Greg" :person/cars [{:car/id 1033 :car/model "Toyota"}]}]}
-                         :replace [:root/list])             ;; in this particular case this :replace do nothing because PersonList is singleton
- ;; - add one car to Greg
- (merge/merge-component! APP Car {:car/id 45 :car/model "F-124"}
-                         :append [:person/id 6 :person/cars])
-
- ;; get ident
- (comp/get-ident Car {:car/id 1})
-
- ;; get initial state
- (comp/get-initial-state Sample)
-
- ;; mutations
- (make-older {:person/id 1})
- `(make-older {:person/id 1})
- (comp/transact! APP [(make-older {:person/id 5})])
- (comp/transact! APP [`(make-older {:person/id 5})])
-
- ;; indexes
- ;; take all components rendering Person
- (comp/class->all APP Person)
- ;; take random component rendering Person
- (comp/class->any APP Person)
-
- ;; show all classes that query a prop
- (comp/prop->classes APP :person/age)
-
- ;; demo
- ;; - get all components querying the prop
- (defn get-component-that-query-for-a-prop [prop]
-   (let [f (fn [p])]
-     (->> (comp/prop->classes APP prop)
-          (reduce (fn [acc cls-key] (concat acc (comp/class->all APP (comp/registry-key->class cls-key)))) []))))
- ;; - find all idents quering :person/age
- (map
-  comp/get-ident
-  (get-component-that-query-for-a-prop :person/age))
- ;; - having ident, get denormalized data of the ident
- (let [state           (app/current-state APP)
-       component-query (comp/get-query Person)
-       component-ident [:person/id 1]
-       starting-entity (get-in state component-ident)]
-   (fdn/db->tree component-query starting-entity state)))
+(comment)
